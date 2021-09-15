1bc2af3f9d2e53c540bb0ac956ff6c676f78a764
Genionest <1281554977@qq.com>
2021-09-15 13:33:07 +0800

    page level up

--- js/book.js
+++ js/book.js
@@ -1,29 +1,48 @@
 $(document).ready(function(){
+    // 目录在这里增加, 对应("page%d-%d.html", page, section)
+    var book = [
+        [
+            "其他",
+            "所需课程",
+            "NCRE相关",
+            "考研相关",
+        ],
+    ];
+
     var bar = $(".sidebar");
-    function get_txt(n){
+    function get_txt(n, m){
         var t = $.ajax({
-            url: "data/page"+n+".html",
+            url: "data/page"+n+"-"+m+".html",
             async: false,
         });
         $(".maincontent").html(t.responseText);
-//         alert(n);
+//         alert("n"+"m");
     };
-    function create_button(n, title){
-        var str = "<div class=\"page_button\" id=\"page"+n+"\">第"+n+"章: "+title+"</div>";
-        bar.append(str);
+    function create_page(n, title){
+        bar.append("<div class='page"+n+"'></div>")
+        var str = "<div class=\"page-btn\" id=\"page"+n+"\">"+title+"</div>";
+        $(".page"+n).append(str);
         $("#page"+n).click(function(){
-            get_txt(n);
+            $("div[id^='section"+n+"']").slideToggle()
         });
     }
-    var book = [
-        "所需课程",
-        "NCRE相关",
-        "考研相关",
-    ];
+    function create_section(n, m, title){
+        var page = $(".page"+n);
+        var str = "<div class=\"section-btn\" id=\"section"+n+"-"+m+"\">"+title+"</div>";
+        page.append(str);
+        $("#section"+n+"-"+m).click(function(){
+            get_txt(n, m);
+        })
+    }
 
     var i = 0;
     while(book[i]){
-        create_button(i+1, book[i]);
+        create_page(i+1, book[i][0]);
+        var j=1;
+        while(book[i][j]){
+            create_section(i+1, j, book[i][j]);
+            j++;
+        }
         i++;
     }
 })
