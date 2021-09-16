d4c26160a360e84b96bf04cc7fed3569aa75817a
Genionest <1281554977@qq.com>
2021-09-16 17:07:23 +0800

    更新保存方式，新增matlab

--- js/book.js
+++ js/book.js
@@ -2,13 +2,22 @@ $(document).ready(function(){
     // 目录在这里增加, 对应("page%d/section%d.html", page, section)
     var book = [
         [
-            "C语言",
+            ["C语言", "c"],
             "链表",
             "交换两个int",
             "一些字符串函数",
         ],
         [
-            "数据结构与算法",
+            ["MATLAB", "matlab"],
+            "变量",
+            "矩阵",
+            "程序结构",
+            "二维平面绘图",
+            "三位离提绘图",
+            "导出图像",
+        ],
+        [
+            ["数据结构与算法", "DandA"],  // 不能用&符号
             "1-1 插入排序",
         ],
     ];
@@ -16,7 +25,7 @@ $(document).ready(function(){
     // 附录在这里增加, 对应("sheet%d/section%d.html", sheet, section)
     var book2 = [
         [
-            "其他",
+            ["其他", "other"],
             "所需课程",
             "NCRE相关",
             "考研相关",
@@ -35,7 +44,7 @@ $(document).ready(function(){
             async: false,
         });
         $(".maincontent").html(t.responseText);
-//         alert("n"+"m");
+        // alert(n+"-"+m);
     };
     function create_page(n, title){
         bar.append("<div class='page"+n+"'></div>")
@@ -72,20 +81,24 @@ $(document).ready(function(){
 
     var i = 0;
     while(book[i]){
-        create_page(i+1, book[i][0]);
+        // create_page(i+1, book[i][0]);
+        create_page(book[i][0][1], book[i][0][0]);
         var j=1;
         while(book[i][j]){
-            create_page_section(i+1, j, book[i][j]);
+            // create_page_section(i+1, j, book[i][j]);
+            create_page_section(book[i][0][1], j, book[i][j]);
             j++;
         }
         i++;
     }
     i = 0;
     while(book2[i]){
-        create_sheet(i+1, book2[i][0]);
+        // create_sheet(i+1, book2[i][0]);
+        create_sheet(book2[i][0][1], book2[i][0][0]);
         var j=1;
         while(book[i][j]){
-            create_sheet_section(i+1, j, book2[i][j]);
+            // create_sheet_section(i+1, j, book2[i][j]);
+            create_sheet_section(book2[i][0][1], j, book2[i][j]);
             j++;
         }
         i++;
