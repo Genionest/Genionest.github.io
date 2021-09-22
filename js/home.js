04415f6ca3f24319247a32a46c3de837628051af
Genionest <1281554977@qq.com>
2021-09-23 00:43:03 +0800

    Update home.js

--- js/home.js
+++ js/home.js
@@ -5,6 +5,15 @@ $(document).ready(function(){
         ["link", "链接"],
     ];
     var header = $(".header");
+    
+    function get_nav(path){
+        var t = $.ajax({
+            url: path,
+            async: false,
+        });
+        $(".maincontent").html(t.responseText);
+        // alert(path);
+    }
     while(menu[i]){
         header.append("<div class='navigation' id='"+menu[i][0]+"'><b>"+menu[i][1]+"<b></div>");
 
@@ -15,14 +24,12 @@ $(document).ready(function(){
         }, function(){
             $(this).css("color", "yellowgreen");
         });
+        // 在click里面的话，只有调用时才初始化，但此时i=2
+        var path = "data/navigation//"+menu[i][0]+".html";
         $("#"+menu[i][0]).click(function(){
             // $(".home").css("color", "yellowgreen");
             // $(this).css("color", "white");
-            var t = $.ajax({
-                url: "data/navigation/"+menu[i][0]+".html",
-                async: false,
-            });
-            $(".maincontent").html(t.responseText);
+            get_nav(path);
         });
         i++;
     }
